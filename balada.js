94ef81aed81f0283fe0033c22262eee71473bf88
LUIS EDUARDO PEREIRA FAVARIS <24250670@educ123.sp.senai.br>
2025-09-10 09:12:02 -0300

    feat: adicionar (rgb piscando balada)

--- balada.js
+++ balada.js
@@ -2,33 +2,28 @@ const RED = 33;
 const GREEN = 27;
 const BLUE = 32;
 
-let r = 0;
-let g = 0;
-let b = 0;
-
 const cores = [
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1],
-    [1, 0, 1],
-    [1, 1, 0],
-    [0, 1, 1],
-    [1, 1, 1],
-]
+    [1, 0, 0], // vermelho
+    [0, 1, 0], // verde
+    [0, 0, 1], // azul
+    [1, 0, 1], // roxo
+    [1, 1, 0], // amarelo
+    [0, 1, 1], // ciano
+    [1, 1, 1], // branco
+];
+
+let i = 0;
 
 setInterval(function () {
-    for (let i = 0; i < cores.length; i++) {
-        const cor = cores[i]
+    const cor = cores[i];
 
-        r = cor[i][0]
-        g = cor[i][1]
-        b = cor[i][2]
+    const r = cor[0];
+    const g = cor[1];
+    const b = cor[2];
 
-        digitalWrite(RED, r);
-        digitalWrite(GREEN, g);
-        digitalWrite(BLUE, b);
-    };
+    digitalWrite(RED, r);
+    digitalWrite(GREEN, g);
+    digitalWrite(BLUE, b);
 
+    i = (i + 1) % cores.length;
 }, 100);
-
-
