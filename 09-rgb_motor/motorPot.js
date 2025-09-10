7b5e9c4ffd2a3988470b2730526946f47c472b15
LUIS EDUARDO PEREIRA FAVARIS <24250670@educ123.sp.senai.br>
2025-09-10 11:21:52 -0300

    feat:adicionar motor modifica de acordo com o pot

---
+++ 09-rgb_motor/motorPot.js
@@ -0,0 +1,17 @@
+const MOTOR =13;
+const POT = 36;
+
+
+pinMode (MOTOR, "output");
+
+
+setInterval(() => {
+
+    let valorPOT =  analogRead(POT) * 100;
+    con
+    let ang = valorPOT;
+    let p = (ang / 18 + 2.5) / 100;
+
+    analogWrite(MOTOR, p, { freq: 50 });
+}, 100)
+
