1c229aa080f52a7aa6ed792de3c0766c3dd0e834
L4lAu <duduzinho172007@gmail.com>
2025-10-22 09:04:25 -0300

    feat: adicionar texto dinamico lcd

--- 16-LCD/lcd.js
+++ 16-LCD/lcd.js
@@ -1,8 +1,19 @@
 I2C1.setup();
 let lcd = require("HD44780").connect12C(I2C1);
 
-lcd.setCursor(0, 0);
-lcd.print("    senai :D    ");
+telas = 0
+setInterval(function () {
+    switch (telas) {
+        case 0:
+            lcd.setCursor(0, 0);
+            lcd.print("    senai :D    ");
+            lcd.setCursor(0, 1);
+            lcd.print(" Armando Arruda ");
 
-lcd.setCursor(0,1);
-lcd.print(" Armando Arruda ")
+        case 1:
+            lcd.setCursor(0, 0);
+            lcd.print("    lalau :D    ");
+            lcd.setCursor(0, 1);
+            lcd.print("   brabissimo   ")
+    }
+},2000)
