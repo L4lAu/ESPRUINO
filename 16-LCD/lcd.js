c53e7cc6e43a1752428a3a6ba5d3e54b93c4f79f
L4lAu <duduzinho172007@gmail.com>
2025-10-22 09:36:22 -0300

    feat: adicionar texto rotativo

--- 16-LCD/lcd.js
+++ 16-LCD/lcd.js
@@ -1,19 +1,19 @@
 I2C1.setup();
-let lcd = require("HD44780").connect12C(I2C1);
+let lcd = require("HD44780").connectI2C(I2C1, 0x27, 20, 4);
 
-telas = 0
-setInterval(function () {
-    switch (telas) {
-        case 0:
-            lcd.setCursor(0, 0);
-            lcd.print("    senai :D    ");
-            lcd.setCursor(0, 1);
-            lcd.print(" Armando Arruda ");
+let texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ";
+let start = 0;
+const width = 16;
 
-        case 1:
-            lcd.setCursor(0, 0);
-            lcd.print("    lalau :D    ");
-            lcd.setCursor(0, 1);
-            lcd.print("   brabissimo   ")
-    }
-},2000)
+texto = texto + " ".repeat(width);
+
+function show() {
+    lcd.setCursor(0, 0);
+    lcd.print(texto.substr(start, width));
+    start++;
+    if (start > texto.length - width) start = 0;
+}
+
+lcd.clear();
+show();
+setInterval(show, 250);
