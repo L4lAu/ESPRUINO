147766882586766f46fb16b2c287e982350c59ae
L4lAu <duduzinho172007@gmail.com>
2025-10-29 10:08:30 -0300

    feat: adicionar AmongUs

---
+++ 17-LCD/caracteres.js
@@ -0,0 +1,21 @@
+I2C1.setup();
+let lcd = require("HD44780").connectI2C(I2C1);
+const POT = 34;
+
+
+let amongus = [
+    0b00110,
+    0b01111,
+    0b11100,
+    0b11111,
+    0b11111,
+    0b01111,
+    0b01001,
+    0b00000
+];
+
+lcd.createChar(0, amongus)
+
+setInterval (function ()  {
+    lcd.write(0)
+ }, 100)
